c58f8649e0617441ccb11513b9d2fa13c8fcb7db
anadahz <andz@torproject.org>
2016-12-06 17:27:37 +0000

    Add osx tests in Travis

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,19 +1,33 @@
 #!/bin/sh
 set -x
 TARGET=$1
+OS_NAME=$2
 DOCKERFILE=Dockerfile.$TARGET
 
-if [ -z $TARGET ];then
-    echo "ERR: you must specify the target"
+if [ -z $OS_NAME ];then
+    echo "ERR: you must specify the OS name"
     exit 1
 fi
 
-if [ ! -f $DOCKERFILE ];then
-    echo "ERR: $DOCKERFILE does not exist"
-    exit 2
-fi
+case "$OS_NAME" in
+    "osx")
+        brew update >/dev/null
+        brew install ooniprobe
+        ./test/run.sh
+        ;;
 
-echo "Building $TARGET ($DOCKERFILE)"
+    "linux")
+        if [ -z $TARGET ];then
+            echo "ERR: you must specify the target"
+            exit 1
+        fi
 
-docker build -t ooniprobe/$TARGET -f $DOCKERFILE .
-docker run ooniprobe/$TARGET /ooniprobe/run.sh
+        if [ ! -f $DOCKERFILE ];then
+            echo "ERR: $DOCKERFILE does not exist"
+            exit 2
+        fi
+
+        echo "Building $TARGET ($DOCKERFILE)"
+
+        docker build -t ooniprobe/$TARGET -f $DOCKERFILE .
+        docker run ooniprobe/$TARGET /ooniprobe/run.sh
