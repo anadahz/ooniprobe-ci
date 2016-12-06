2ca3d930f6efc70e5b0ebbb3937aca61412f4031
anadahz <andz@torproject.org>
2016-12-06 18:15:28 +0000

    Remove linux os env we are using docker

--- build-and-run.sh
+++ build-and-run.sh
@@ -4,19 +4,13 @@ TARGET=$1
 OS_NAME=$2
 DOCKERFILE=Dockerfile.$TARGET
 
-if [ -z $OS_NAME ];then
-    echo "ERR: you must specify the OS name"
-    exit 1
-fi
-
 case "$OS_NAME" in
     "osx")
         brew update >/dev/null
         brew install ooniprobe
         ./test/run.sh
         ;;
-
-    "linux")
+    *)
         if [ -z $TARGET ];then
             echo "ERR: you must specify the target"
             exit 1
@@ -31,9 +25,5 @@ case "$OS_NAME" in
 
         docker build -t ooniprobe/$TARGET -f $DOCKERFILE .
         docker run ooniprobe/$TARGET /ooniprobe/run.sh
-        ;;
-    *)
-        echo "Invalid or unsupported operating system ${OS_NAME}"
-        exit 1
         ;;
    esac
