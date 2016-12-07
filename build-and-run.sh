5c241d56b6cbfd68f31057408a427ac788855cc2
anadahz <andz@torproject.org>
2016-12-07 11:49:36 +0000

    Fix build run

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,35 +1,30 @@
 #!/bin/sh
 set -x
-TARGET=$1
-OS_NAME=$2
-DOCKERFILE=Dockerfile.$TARGET
+OS_NAME=$1
+TARGET=$2
+DOCKERFILE=Dockerfile.${TARGET}
 
-if [ -z $OS_NAME ];then
-    echo "ERR: you must specify the OS name"
-    exit 1
-fi
-
-case "$OS_NAME" in
+case "${OS_NAME}" in
     "osx")
         brew update >/dev/null
         brew install ooniprobe
         ./test/run.sh
         ;;
     "linux")
-        if [ -z $TARGET ];then
+        if [ -z ${TARGET} ];then
             echo "ERR: you must specify the target"
             exit 1
         fi
 
-        if [ ! -f $DOCKERFILE ];then
-            echo "ERR: $DOCKERFILE does not exist"
+        if [ ! -f ${DOCKERFILE} ];then
+            echo "ERR: ${DOCKERFILE} does not exist"
             exit 2
         fi
 
-        echo "Building $TARGET ($DOCKERFILE)"
+        echo "Building ${TARGET} (${DOCKERFILE})"
 
-        docker build -t ooniprobe/$TARGET -f $DOCKERFILE .
-        docker run ooniprobe/$TARGET /ooniprobe/run.sh
+        docker build -t ooniprobe/${TARGET} -f ${DOCKERFILE} .
+        docker run ooniprobe/${TARGET} /ooniprobe/run.sh
         ;;
     *)
         echo "Invalid or unsupported operating system ${OS_NAME}"
