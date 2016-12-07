f9fa6b03f40d12a3d254d74788180ee6f766a757
anadahz <andz@torproject.org>
2016-12-07 01:16:01 +0000

    Fix matrix

--- build-and-run.sh
+++ build-and-run.sh
@@ -4,13 +4,18 @@ TARGET=$1
 OS_NAME=$2
 DOCKERFILE=Dockerfile.$TARGET
 
+if [ -z $OS_NAME ];then
+    echo "ERR: you must specify the OS name"
+    exit 1
+fi
+
 case "$OS_NAME" in
     "osx")
         brew update >/dev/null
         brew install ooniprobe
         ./test/run.sh
         ;;
-    *)
+    "linux")
         if [ -z $TARGET ];then
             echo "ERR: you must specify the target"
             exit 1
@@ -25,5 +30,9 @@ case "$OS_NAME" in
 
         docker build -t ooniprobe/$TARGET -f $DOCKERFILE .
         docker run ooniprobe/$TARGET /ooniprobe/run.sh
+        ;;
+    *)
+        echo "Invalid or unsupported operating system ${OS_NAME}"
+        exit 1
         ;;
    esac
